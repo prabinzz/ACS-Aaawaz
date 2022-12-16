20cef5add8c713fa1aafb7e56dfcb47bcc5a0b17
Prabin Parajuli <prabinparajuli92@gmil.com>
2022-12-16 19:25:49 +0545

    Combined-Stable

--- App.js
+++ App.js
@@ -7,12 +7,16 @@ import { Interaction } from "./Components/Interaction";
 import { Helpline } from "./Components/Helpline";
 import NepaliNumbers from "./Components/NepaliNumbers";
 import { NepaliAlphabate } from "./Components/NepaliAlphabate";
+import Learning from "./Components/Learning";
+import EnglishAlphabes from "./Components/EnglishAlphabets";
 
 const App = () => {
   const Stack = createNativeStackNavigator();
   return (
     <NavigationContainer>
       <Stack.Navigator>
+        <Stack.Screen name="EnglishAlphabets" component={EnglishAlphabes} />
+        <Stack.Screen name="Learning" component={Learning} />
         <Stack.Screen name="Helpline" component={Helpline} />
         <Stack.Screen name="Interaction" component={Interaction} />
         <Stack.Screen name="NeplaiNumbers" component={NepaliNumbers} />
